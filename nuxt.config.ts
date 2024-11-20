64b45e947d460c907a81154a6407a40dff8e09e3
Dylan K <50585244+dylankainth@users.noreply.github.com>
2024-11-20 14:52:41 +0000

    added bootstrap ?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.min.css'
+  ],
+  script: [
+    {
+      src: 'bootstrap/dist/js/bootstrap.bundle.min.js'
+    }
+  ],
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
