beadb73a3a96f64383c86d4297a47201da15f7b2
Dylan K <50585244+dylankainth@users.noreply.github.com>
2024-11-20 15:38:14 +0000

    progress 15:38

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  googleFonts: {
+    families: {
+      'GFS+Didot': true,
+      'montserrat' : true
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  modules: ['@nuxtjs/google-fonts'],
   css: [
     'bootstrap/dist/css/bootstrap.min.css'
   ],
-  script: [
-    {
-      src: 'bootstrap/dist/js/bootstrap.bundle.min.js'
-    }
-  ],
+  head: {
+    script: [
+      {
+        src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
+        type: "text/javascript",
+      },
+    ],
+  },
   vite: {
     define: {
       'process.env.DEBUG': false,
---
+++ pages/mentorshipHub.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>mentorship</div>
+</template>
